e91aad74727d5269072139ea0f0dc0a0b52ccc0f
Jeremias Alvarez <jere_pomelo122@hotmail.com>
2020-05-12 01:56:09 -0300

    nuevo js

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@
             let calcular = document.getElementById('calcular');
             let errorDiv = document.getElementById('error');
             let registro = document.getElementById('btnRegistro');
-            let resultado = document.getElementById('lista-productos');
-
+            let listaProductosElement = document.getElementById('lista-productos');
+            let totalElement = document.getElementById('suma-total');
 
             //extras
 
@@ -51,18 +51,56 @@
                     return;
                 }
 
-                let boletoDia = paseDia.value,
-                    boleto2Dias = pase2Dias.value,
-                    boletoCompleto = paseCompleto.value,
-                    cantCamisas = camiseta.value,
-                    cantEtiquetas = etiqueta.value;
+                let boletoDia = parseInt(paseDia.value, 10) || 0,
+                    boleto2Dias = parseInt(pase2Dias.value, 10) || 0,
+                    boletoCompleto = parseInt(paseCompleto.value, 10) || 0,
+                    cantCamisas = parseInt(camiseta.value, 10) || 0,
+                    cantEtiquetas = parseInt(etiqueta.value, 10) || 0;
 
                 let total = (boletoDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50) +
                     ((cantCamisas * 10) * 0.93) + (cantEtiquetas * 10);
 
-                console.log(total);
+                let listaProducto = [];
 
 
+                if (boletoDia >= 1)
+                    listaProducto.push(`${boletoDia} pases para 1 dia`);
+
+                if (boleto2Dias >= 1)
+                    listaProducto.push(`${boleto2Dias} pases para 2 dias`);
+
+                if (boletoCompleto >= 1)
+                    listaProducto.push(`${boletoCompleto} pases completos`);
+
+                if (cantCamisas >= 1)
+                    listaProducto.push(`${cantCamisas} camisetas`);
+
+                if (cantEtiquetas >= 1)
+                    listaProducto.push(`${cantEtiquetas} etiquetas`);
+
+
+                listaProductosElement.style.display = 'block';
+                listaProductosElement.innerHTML = '';
+
+                listaProducto.forEach(producto => {
+                    let nuevoElemento = createProductParagraph(producto);
+                    listaProductosElement.appendChild(nuevoElemento);
+                })
+
+
+                totalElement.innerHTML = '';
+                total = total.toFixed(2);
+                let precio = document.createElement('h5');
+                precio.innerText = `$${total}`;
+
+                totalElement.appendChild(precio);
+            }
+
+
+            function createProductParagraph(element) {
+                let p = document.createElement('h5');
+                p.innerText = element;
+                return p;
             }
 
 
